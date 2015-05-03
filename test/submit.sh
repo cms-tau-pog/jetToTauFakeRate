a5dc61865e1b39f63c1edf85889a9e738e8deeff
Pietro Vischia <pietro.vischia@gmail.com>
2015-05-03 19:12:30 +0200

    Added cleanup of working directory to the submission script

--- test/submit.sh
+++ test/submit.sh
@@ -3,6 +3,9 @@
 JSONFILE=$CMSSW_BASE/src/TauAnalysis/JetToTauFakeRate/data/samples.json
 OUTDIR=$CMSSW_BASE/src/TauAnalysis/JetToTauFakeRate/test/results
 
+# cleanup
+rm -r ${OUTDIR}
+# recreate
 mkdir -p ${OUTDIR}
 
 runLocalAnalysisOverSamples.py -e runTauFakesStudy -j ${JSONFILE} -o ${OUTDIR} -d  /dummy/ -c $CMSSW_BASE/src/TauAnalysis/JetToTauFakeRate/test/runAnalysis_cfg.py.templ -p "@useMVA=False @saveSummaryTree=False @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0" -s 8nh
