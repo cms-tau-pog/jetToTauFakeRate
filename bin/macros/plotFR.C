3615b6d5b4071e852fc37764b661147e4edebfc8
Pietro Vischia <pietro.vischia@gmail.com>
2015-05-04 16:12:46 +0200

    Colour scheme as in TAU-14-008, in the preliminary plotter
    Also, pt plot rebinned with variable bin width

--- bin/macros/plotFR.C
+++ bin/macros/plotFR.C
@@ -4,39 +4,47 @@ void plotFR(){
   analyses.push_back("wjet");
   analyses.push_back("qcd");
 
+  Double_t ptbins[20] = { 0., 10., 20., 30., 40., 50., 60., 70., 80., 90., 100., 120., 140., 160., 180., 200., 250., 300., 400., 500.};
+  //vector<int> colours;
+  vector<Int_t> colours;
+  vector<Int_t> markers;
+  
+  
+
+
   vector<TString> tauDiscriminators; tauDiscriminators.clear();
 
-  tauDiscriminators.push_back("byLooseCombinedIsolationDeltaBetaCorr3Hits");
-  tauDiscriminators.push_back("byMediumCombinedIsolationDeltaBetaCorr3Hits");
-  tauDiscriminators.push_back("byTightCombinedIsolationDeltaBetaCorr3Hits");
+  tauDiscriminators.push_back("byLooseCombinedIsolationDeltaBetaCorr3Hits"); colours.push_back(TColor::GetColor("#009900"));markers.push_back(25/*21data*/);
+  tauDiscriminators.push_back("byMediumCombinedIsolationDeltaBetaCorr3Hits");colours.push_back(TColor::GetColor("#ff6600"));markers.push_back(26/*22data*/);
+  tauDiscriminators.push_back("byTightCombinedIsolationDeltaBetaCorr3Hits"); colours.push_back(TColor::GetColor("#990099"));markers.push_back(32/*23data*/);
                           
-  tauDiscriminators.push_back("byVLooseIsolationMVA3oldDMwoLT");
-  tauDiscriminators.push_back("byLooseIsolationMVA3oldDMwoLT");
-  tauDiscriminators.push_back("byMediumIsolationMVA3oldDMwoLT");
-  tauDiscriminators.push_back("byTightIsolationMVA3oldDMwoLT");
-  tauDiscriminators.push_back("byVTightIsolationMVA3oldDMwoLT");
-  tauDiscriminators.push_back("byVVTightIsolationMVA3oldDMwoLT");
-  
-  tauDiscriminators.push_back("byVLooseIsolationMVA3oldDMwLT");
-  tauDiscriminators.push_back("byLooseIsolationMVA3oldDMwLT");
-  tauDiscriminators.push_back("byMediumIsolationMVA3oldDMwLT");
-  tauDiscriminators.push_back("byTightIsolationMVA3oldDMwLT");
-  tauDiscriminators.push_back("byVTightIsolationMVA3oldDMwLT");
-  tauDiscriminators.push_back("byVVTightIsolationMVA3oldDMwLT");
-  
-  tauDiscriminators.push_back("byVLooseIsolationMVA3newDMwoLT");
-  tauDiscriminators.push_back("byLooseIsolationMVA3newDMwoLT");
-  tauDiscriminators.push_back("byMediumIsolationMVA3newDMwoLT");
-  tauDiscriminators.push_back("byTightIsolationMVA3newDMwoLT");
-  tauDiscriminators.push_back("byVTightIsolationMVA3newDMwoLT");
-  tauDiscriminators.push_back("byVVTightIsolationMVA3newDMwoLT");
-  
-  tauDiscriminators.push_back("byVLooseIsolationMVA3newDMwLT");
-  tauDiscriminators.push_back("byLooseIsolationMVA3newDMwLT");
-  tauDiscriminators.push_back("byMediumIsolationMVA3newDMwLT");
-  tauDiscriminators.push_back("byTightIsolationMVA3newDMwLT");
-  tauDiscriminators.push_back("byVTightIsolationMVA3newDMwLT");
-  tauDiscriminators.push_back("byVVTightIsolationMVA3newDMwLT");
+//  tauDiscriminators.push_back("byVLooseIsolationMVA3oldDMwoLT");  colours.push_back(2);
+//  tauDiscriminators.push_back("byLooseIsolationMVA3oldDMwoLT");   colours.push_back(3);
+//  tauDiscriminators.push_back("byMediumIsolationMVA3oldDMwoLT");  colours.push_back(4);
+//  tauDiscriminators.push_back("byTightIsolationMVA3oldDMwoLT");   colours.push_back(2);
+//  tauDiscriminators.push_back("byVTightIsolationMVA3oldDMwoLT");  colours.push_back(3);
+//  tauDiscriminators.push_back("byVVTightIsolationMVA3oldDMwoLT"); colours.push_back(4);
+//  
+//  tauDiscriminators.push_back("byVLooseIsolationMVA3oldDMwLT");   colours.push_back(2);
+//  tauDiscriminators.push_back("byLooseIsolationMVA3oldDMwLT");    colours.push_back(3);
+//  tauDiscriminators.push_back("byMediumIsolationMVA3oldDMwLT");   colours.push_back(4);
+//  tauDiscriminators.push_back("byTightIsolationMVA3oldDMwLT");    colours.push_back(2);
+//  tauDiscriminators.push_back("byVTightIsolationMVA3oldDMwLT");   colours.push_back(3);
+//  tauDiscriminators.push_back("byVVTightIsolationMVA3oldDMwLT");  colours.push_back(4);
+//  
+//  tauDiscriminators.push_back("byVLooseIsolationMVA3newDMwoLT");  colours.push_back(2);
+//  tauDiscriminators.push_back("byLooseIsolationMVA3newDMwoLT");   colours.push_back(3);
+//  tauDiscriminators.push_back("byMediumIsolationMVA3newDMwoLT");  colours.push_back(4);
+//  tauDiscriminators.push_back("byTightIsolationMVA3newDMwoLT");   colours.push_back(2);
+//  tauDiscriminators.push_back("byVTightIsolationMVA3newDMwoLT");  colours.push_back(3);
+//  tauDiscriminators.push_back("byVVTightIsolationMVA3newDMwoLT"); colours.push_back(4);
+//  
+//  tauDiscriminators.push_back("byVLooseIsolationMVA3newDMwLT");   colours.push_back(2);
+//  tauDiscriminators.push_back("byLooseIsolationMVA3newDMwLT");    colours.push_back(3);
+//  tauDiscriminators.push_back("byMediumIsolationMVA3newDMwLT");   colours.push_back(4);
+//  tauDiscriminators.push_back("byTightIsolationMVA3newDMwLT");    colours.push_back(2);
+//  tauDiscriminators.push_back("byVTightIsolationMVA3newDMwLT");   colours.push_back(3);
+//  tauDiscriminators.push_back("byVVTightIsolationMVA3newDMwLT");  colours.push_back(4);
 
   for(vector<TString>::iterator anal=analyses.begin(); anal!=analyses.end(); ++anal)
     {
@@ -106,11 +114,18 @@ void plotFR(){
           temp_eta_numerator   ->Sumw2();
           temp_radius_numerator->Sumw2();
           temp_nvtx_numerator  ->Sumw2();
+
+
+          TH1* temp_ptbinned = temp_pt_numerator    ->Rebin(19, "", ptbins);          
+          temp_pt_numerator=temp_ptbinned;
+          temp_eta_numerator   ->Rebin(2);
+          temp_radius_numerator->Rebin(2);
+          temp_nvtx_numerator  ->Rebin(2);
           
-          temp_pt_numerator    ->SetMarkerColor(10*l+l+1);
-          temp_eta_numerator   ->SetMarkerColor(10*l+l+1);
-          temp_radius_numerator->SetMarkerColor(10*l+l+1);
-          temp_nvtx_numerator  ->SetMarkerColor(10*l+l+1);
+          temp_pt_numerator    ->SetMarkerColor(colours[l]);temp_pt_numerator    ->SetLineColor(colours[l]);temp_pt_numerator    ->SetLineWidth(2);temp_pt_numerator    ->SetMarkerStyle(markers[l]);temp_pt_numerator    ->SetMarkerSize(1.5);
+          temp_eta_numerator   ->SetMarkerColor(colours[l]);temp_eta_numerator   ->SetLineColor(colours[l]);temp_eta_numerator   ->SetLineWidth(2);temp_eta_numerator   ->SetMarkerStyle(markers[l]);temp_eta_numerator   ->SetMarkerSize(1.5);
+          temp_radius_numerator->SetMarkerColor(colours[l]);temp_radius_numerator->SetLineColor(colours[l]);temp_radius_numerator->SetLineWidth(2);temp_radius_numerator->SetMarkerStyle(markers[l]);temp_radius_numerator->SetMarkerSize(1.5);
+          temp_nvtx_numerator  ->SetMarkerColor(colours[l]);temp_nvtx_numerator  ->SetLineColor(colours[l]);temp_nvtx_numerator  ->SetLineWidth(2);temp_nvtx_numerator  ->SetMarkerStyle(markers[l]);temp_nvtx_numerator  ->SetMarkerSize(1.5);
           
           pt_numerator    .push_back(temp_pt_numerator    );
           eta_numerator   .push_back(temp_eta_numerator   );
@@ -123,8 +138,13 @@ void plotFR(){
       if(!eta_denominator   ) cout << " eta_denominator    is NULL" << endl; else eta_denominator   ->Sumw2();
       if(!radius_denominator) cout << " radius_denominator is NULL" << endl; else radius_denominator->Sumw2();
       if(!nvtx_denominator  ) cout << " nvtx_denominator   is NULL" << endl; else nvtx_denominator  ->Sumw2();
-      
-      
+
+      TH1* pt_binned = pt_denominator    ->Rebin(19, "", ptbins);
+      pt_denominator=pt_binned;
+      eta_denominator   ->Rebin(2);
+      radius_denominator->Rebin(2);
+      nvtx_denominator  ->Rebin(2);      
+
       for(size_t l=0; l<tauDiscriminators.size(); ++l)
         {
           pt_numerator    [l]->Divide( pt_denominator     );
@@ -132,7 +152,7 @@ void plotFR(){
           radius_numerator[l]->Divide( radius_denominator );
           nvtx_numerator  [l]->Divide( nvtx_denominator   );
         }
-      
+
       TCanvas* c = new TCanvas("fakerate", "fakerate", 800, 800);
       c->Divide(2,2);
       c->cd(1);
