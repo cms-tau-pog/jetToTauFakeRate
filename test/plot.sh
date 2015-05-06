c2121fadf5fa9e72f9ddeaf48ca53213c0aa391c
Pietro Vischia <pietro.vischia@gmail.com>
2015-05-06 11:48:57 +0200

    Produce only needed plots

--- test/plot.sh
+++ test/plot.sh
@@ -2,31 +2,35 @@
 
 DIR=~/www/13TeV_tauFakes/
 mkdir -p ${DIR}
-rm -rf ${DIR}*
+mkdir -p ~/www/temptemp/
+mv ${DIR}*vischia*pdf ~/www/temptemp/
+rm -r ${DIR}*
+mv ~/www/temptemp/* ${DIR}
 cp ~/www/HIG-13-026/index.php ${DIR}
 
-LUMI=7318
+LUMIWJETS=700
+LUMIQCD=4
 JSONFILEWJETS=$CMSSW_BASE/src/TauAnalysis/JetToTauFakeRate/data/wjets_samples.json
 JSONFILEQCD=$CMSSW_BASE/src/TauAnalysis/JetToTauFakeRate/data/qcd_samples.json
 INDIR=$CMSSW_BASE/src/TauAnalysis/JetToTauFakeRate/test/results/
 PLOTTERWJETS=${INDIR}plotter_wjet.root
 PLOTTERQCD=${INDIR}plotter_qcd.root
-ONLYWJETS=""
-ONLYQCD=""
+ONLYWJETS="--onlyStartsWith wjet"
+ONLYQCD="--onlyStartsWith qcd"
 
 
 ## Create plotter files from which the ratio for fake rate will be computed
 # WJets
-runFixedPlotter --iEcm 13 --iLumi ${LUMI} --inDir ${INDIR} --outDir ${DIR} --outFile ${PLOTTERWJETS} --json ${JSONFILEWJETS} --cutflow all_initNorm --forceMerge --no2D --noPowers --plotExt .png ${ONLYWJETS}
+runFixedPlotter --iEcm 13 --iLumi ${LUMIWJETS} --inDir ${INDIR} --outDir ${DIR} --outFile ${PLOTTERWJETS} --json ${JSONFILEWJETS} --cutflow all_initNorm --forceMerge --no2D --noPowers --plotExt .png ${ONLYWJETS}
 # QCD
-runFixedPlotter --iEcm 13 --iLumi ${LUMI} --inDir ${INDIR} --outDir ${DIR} --outFile ${PLOTTERQCD}   --json ${JSONFILEQCD}   --cutflow all_initNorm --forceMerge --no2D --noPowers --plotExt .png ${ONLYQCD}
+runFixedPlotter --iEcm 13 --iLumi ${LUMIQCD} --inDir ${INDIR} --outDir ${DIR} --outFile ${PLOTTERQCD}   --json ${JSONFILEQCD}   --cutflow all_initNorm --forceMerge --no2D --noPowers --plotExt .png ${ONLYQCD}
 
 # Now run test/harvest.sh by hand please
 
 
+exit 0
 # Lessen the burden on the web browser
-
-for CHAN in emu ee mumu singlemu singlee
+for CHAN in qcd wjet
 do
     mkdir ${DIR}temp${CHAN}/
     mv ${DIR}${CHAN}* ${DIR}temp${CHAN}/
