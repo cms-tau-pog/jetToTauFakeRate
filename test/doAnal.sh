8b6c0508c80904614c7db93194d1a29e4d49ca74
Pietro Vischia <pietro.vischia@gmail.com>
2015-07-23 00:56:24 +0200

    Obsolete lumi -> now using lcr2 script

--- test/doAnal.sh
+++ test/doAnal.sh
@@ -17,6 +17,8 @@ if [ "${1}" = "submit" ]; then
     runAnalysisOverSamples.py -e runTauFakesStudy -j ${JSONFILE} -o ${OUTDIR} -d  /dummy/ -c $CMSSW_BASE/src/TauAnalysis/JetToTauFakeRate/test/runAnalysis_cfg.py.templ -p "@useMVA=False @saveSummaryTree=False @runSystematics=False @automaticSwitch=False @is2011=False @jacknife=0 @jacks=0" -s 8nh
     
 elif [ "${1}" = "lumi" ]; then
+    echo "PLEASE USE the lcr2 script." 
+    exit 0
     JSONFILE=$CMSSW_BASE/src/TauAnalysis/JetToTauFakeRate/data/data_samples.json
     OUTDIR=$CMSSW_BASE/src/TauAnalysis/JetToTauFakeRate/test/results_lumi
     # cleanup (comment it out if you have smaller jsons for running only on a few sets while the others are OK
